e7a3d5ca47f83b36742df36c4077938cdd5ad169
Luis Meirelles <luis.gustavo.souto.meirelles@gmail.com>
2025-08-03 03:32:22 -0300

    refactor(app.module): remove duplicate module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,6 @@
 import { Module } from '@nestjs/common';
 import { SubscriptionsModule } from './subscriptions/subscriptions.module';
 import { PersonModule } from './person/person.module';
-import { PersonModule } from './person/person.module';
-import { PersonModule } from './person/person.module';
-import { SubscriptionsModule } from './subscriptions/subscriptions.module';
 
 @Module({
   imports: [SubscriptionsModule, PersonModule],
